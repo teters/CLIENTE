edc7b3e49fab3a123e8b8913712876232cb400f0
teters <mpatrone@correo.um.edu.uy>
2022-10-14 17:35:54 -0300

    getMaping de empresas

--- PrimerProyectoTIC1/src/main/java/com/example/PrimerProyectoTIC1/CentroDeportivoP/CrearCentroDepController.java
+++ PrimerProyectoTIC1/src/main/java/com/example/PrimerProyectoTIC1/CentroDeportivoP/CrearCentroDepController.java
@@ -1,9 +1,6 @@
 package com.example.PrimerProyectoTIC1.CentroDeportivoP;
 
 import com.google.gson.Gson;
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
-import com.mashape.unirest.http.Unirest;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -12,6 +9,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
 
 
 public class CrearCentroDepController {
@@ -27,7 +27,7 @@ public class CrearCentroDepController {
         CentroDeportivo centroDeportivo = new CentroDeportivo(nombreCentroDep.getText(),direccionCentroDep.getText());
         Gson gson=new Gson();
         String body= gson.toJson(centroDeportivo);
-        //HttpResponse<JsonNode> response= Unirest.post(("http://localhost:8080/empresa/listaDeCentrosDeportivos").header("Content-Type","application/json").body(new JsonNode(body)).asJson());
+        HttpResponse<kong.unirest.JsonNode> jsonNodeHttpResponse= Unirest.post("http://localhost:8080/centrodeportivo/agregarcentrodeportivo").header("Content-Type","application/json").body(new JsonNode(body)).asJson();
         FXMLLoader loader = new FXMLLoader();
         Parent root = loader.load(getClass().getResource("option-pane.fxml"));
         Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
