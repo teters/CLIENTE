e04a04eaf432255f0aae4e1a3754d1471007f643
MartinLasala <mlasala1228@gmail.com>
2022-10-14 21:25:40 -0300

    Empleado final view

--- PrimerProyectoTIC1/src/main/java/com/example/PrimerProyectoTIC1/EmpleadoP/VentanaActividadController.java
+++ PrimerProyectoTIC1/src/main/java/com/example/PrimerProyectoTIC1/EmpleadoP/VentanaActividadController.java
@@ -1,4 +1,17 @@
 package com.example.PrimerProyectoTIC1.EmpleadoP;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
 public class VentanaActividadController {
+
+
+    @FXML
+    private ImageView imgAct;
+
+
+
+
+
 }
--- PrimerProyectoTIC1/src/main/java/com/example/PrimerProyectoTIC1/EmpleadoP/VistaEmpleadoController.java
+++ PrimerProyectoTIC1/src/main/java/com/example/PrimerProyectoTIC1/EmpleadoP/VistaEmpleadoController.java
@@ -2,4 +2,9 @@ package com.example.PrimerProyectoTIC1.EmpleadoP;
 
 public class VistaEmpleadoController {
 
+
+
+
+
+
 }
