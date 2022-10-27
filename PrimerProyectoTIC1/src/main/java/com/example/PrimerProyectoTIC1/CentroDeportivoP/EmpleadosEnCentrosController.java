3fe81afa9196c4c9460cdd3f959d31f6de7a0cb3
MartinLasala <mlasala1228@gmail.com>
2022-10-27 12:04:03 -0300

    Interfaz de empleados-en-centros.fxml(TM) encaminada.

--- PrimerProyectoTIC1/src/main/java/com/example/PrimerProyectoTIC1/CentroDeportivoP/EmpleadosEnCentrosController.java
+++ PrimerProyectoTIC1/src/main/java/com/example/PrimerProyectoTIC1/CentroDeportivoP/EmpleadosEnCentrosController.java
@@ -1,15 +1,54 @@
 package com.example.PrimerProyectoTIC1.CentroDeportivoP;
 
-public class EmpleadosEnCentrosController {
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 
-    public void chequearReserva(Long id){
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class EmpleadosEnCentrosController implements Initializable {
+
+    @FXML
+    private ChoiceBox<String> actividadesList;
+
+    @FXML
+    private DatePicker fecha;
+
+    @FXML
+    private ChoiceBox<String> horarios;
+
+    @FXML
+    private TextField mailDeEmp;
+
+    List<String> listaActividades = new ArrayList<>();
+
+
+    public void setData(Actividad actividad){
 
 
 
     }
 
+    public void validar(ActionEvent event){
 
 
 
 
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+
+
+    }
 }
