cc91302888a338edb4d17327c6ca5cee53acb71d
teters <mpatrone@correo.um.edu.uy>
2022-10-28 18:26:19 -0300

    alta actividad

--- PrimerProyectoTIC1/src/main/java/com/example/PrimerProyectoTIC1/CentroDeportivoP/CrearActividadController.java
+++ PrimerProyectoTIC1/src/main/java/com/example/PrimerProyectoTIC1/CentroDeportivoP/CrearActividadController.java
@@ -1,6 +1,18 @@
 package com.example.PrimerProyectoTIC1.CentroDeportivoP;
 
+import com.google.gson.Gson;
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+
 public class CrearActividadController {
+    public void altaActividad(Actividad actividad){
+        Gson gson=new Gson();
+        String body= gson.toJson(actividad);
+        HttpResponse<JsonNode> response = Unirest.post("http://localhost:8080/empleado/iniciosesion").
+                header("Content-Type","application/json").
+                body(new JsonNode(body)).asJson();
+    }
 
 
     //Rellenar, eventualmente.
