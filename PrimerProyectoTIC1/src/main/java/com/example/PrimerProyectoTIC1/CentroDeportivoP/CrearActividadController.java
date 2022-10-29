a211fb38d9ab19fb0a27c621d734b53a92b35bd1
teters <mpatrone@correo.um.edu.uy>
2022-10-28 21:30:04 -0300

    Imageneish

--- PrimerProyectoTIC1/src/main/java/com/example/PrimerProyectoTIC1/CentroDeportivoP/CrearActividadController.java
+++ PrimerProyectoTIC1/src/main/java/com/example/PrimerProyectoTIC1/CentroDeportivoP/CrearActividadController.java
@@ -1,9 +1,18 @@
 package com.example.PrimerProyectoTIC1.CentroDeportivoP;
 
+import com.example.PrimerProyectoTIC1.Imagen;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.List;
 
 public class CrearActividadController {
     public void altaActividad(Actividad actividad){
@@ -12,6 +21,10 @@ public class CrearActividadController {
         HttpResponse<JsonNode> response = Unirest.post("http://localhost:8080/empleado/iniciosesion").
                 header("Content-Type","application/json").
                 body(new JsonNode(body)).asJson();
+    }
+    public void subirFotos(Actividad actividad){
+
+
     }
 
 
