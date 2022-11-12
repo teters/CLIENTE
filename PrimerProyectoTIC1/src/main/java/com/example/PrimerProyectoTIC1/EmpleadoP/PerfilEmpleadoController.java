be338674a8c6660673167bcdfa4c0fd6256a0a17
MartinLasala <mlasala1228@gmail.com>
2022-11-11 21:29:51 -0300

    Un commit como tantos otros

--- PrimerProyectoTIC1/src/main/java/com/example/PrimerProyectoTIC1/EmpleadoP/PerfilEmpleadoController.java
+++ PrimerProyectoTIC1/src/main/java/com/example/PrimerProyectoTIC1/EmpleadoP/PerfilEmpleadoController.java
@@ -1,7 +1,58 @@
 package com.example.PrimerProyectoTIC1.EmpleadoP;
 
-public class PerfilEmpleadoController {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+import org.apache.http.HttpRequest;
 
-//    fx:controller="com.example.PrimerProyectoTIC1.EmpleadoP.PerfilEmpleadoController"
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class PerfilEmpleadoController implements Initializable {
+
+
+    @FXML
+    private Label empresa;
+
+    @FXML
+    private Label fechaVenc;
+
+    @FXML
+    private Label mail;
+
+    @FXML
+    private Label nombreEmpleado;
+
+    @FXML
+    private Label saldo;
+
+    @FXML
+    private Label telefono;
+
+    @FXML
+    void salir(ActionEvent event) {
+
+    }
+
+    @FXML
+    void volver(ActionEvent event) {
+
+    }
+
+
+    public Empleado obtenerEmpleadoDelLogin(){
+        //Teo, aca haceme esta response si podes :D
+
+        return null;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
